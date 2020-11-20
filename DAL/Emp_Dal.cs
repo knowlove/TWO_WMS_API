23dfda4889ef061e91ce01eeccddf8e336fb3322
刘浩闯小盆友 <@L>
2020-11-20 18:42:24 +0800

    22

---
+++ DAL/Emp_Dal.cs
@@ -0,0 +1,79 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class Emp_Dal
+    {
+
+        public List<Emp_Model> Show(string Cgdh, string Gys, string Pl, string Cgr, string Rkzt)
+        {
+
+            string sql = string.Format("select * from Dh_Table where 1=1");
+            if (Cgdh!="")
+            {
+
+                sql += string.Format(" and DhCgdh like '%{0}%'", Cgdh);
+
+            }
+            if (Gys!="")
+            {
+                sql += string.Format(" and DhGys like '%{0}%'", Gys);
+            }
+            if (Pl!="")
+            {
+                sql += string.Format(" and DhPl like '%{0}%'", Pl);
+            }
+            if (Cgr!="")
+            {
+                sql += string.Format(" and DhName like '%{0}%'", Cgr);
+            }
+            if (Rkzt!="")
+            {
+                sql += string.Format(" and DhType like '%{0}%'", Rkzt);
+            }
+            return DBHelper.GetList<Emp_Model>(sql);
+
+
+        }
+        public List<Emp_Model> GetAll()
+        {
+
+            string sql = string.Format("select * from Dh_Table");
+            return DBHelper.GetList<Emp_Model>(sql);
+
+        }
+        public List<Emp_Model> GetXqy(int id)
+        {
+
+            string sql = string.Format("select * from Dh_Table where DhId = {0}", id);
+            return DBHelper.GetList<Emp_Model>(sql);
+
+        }
+        public int DelDhEmp(int id)
+        {
+
+            string sql = string.Format("delete from Dh_Table where DhId = {0}", id);
+            return DBHelper.ExecuteNonQuery(sql);
+
+        }
+        public int UpEmp(string Gys,string Xhqy, string Cgsl, string Cgdj, string Je, string Sl, string Rq,string Bz,string id)
+        {
+
+            string sql = string.Format("update Dh_Table set DhGys='{0}',DhHq ='{1}' ,DhCgsl='{2}',DhJe='{3}',DhSl='{4}',DhRq='{5}',DhBz='{6}',DhDj='{7}' where DhId='{8}'", Gys, Xhqy, Cgsl, Cgdj, Je, Sl, Rq, Bz,Cgdj, id);
+            return DBHelper.ExecuteNonQuery(sql);
+
+        }
+        public int AddDhEmp(string DhCgdh, string DhGys, string DhPl, string DhRq, string DhName, string DhType, string DhYlbm, string DhYlmc, string DhWq, string DhYlGg, string DhCgsl, string DhBz, string DhHq, string DhJe, string DhSl, string DhDj)
+        {
+            string sql = string.Format("insert into Dh_Table values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}','{13}','{14}','{15}')", DhCgdh, DhGys, DhPl, DhRq, DhName, DhType, DhYlbm, DhYlmc, DhWq, DhYlGg, DhCgsl, DhBz, DhHq, DhJe, DhSl, DhDj);
+            return DBHelper.ExecuteNonQuery(sql);
+
+        }
+
+    }
+}
---
+++ TWO_WMS_API/Controllers/DhEmpController.cs
@@ -0,0 +1,70 @@
+﻿using Dal;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TWO_WMS_API.Controllers
+{
+    public class DhEmpController : ApiController
+    {
+        // GET: api/DhEmp
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/DhEmp/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/DhEmp
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/DhEmp/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/DhEmp/5
+        public void Delete(int id)
+        {
+        }
+        Emp_Dal ed = new Emp_Dal();
+        // GET api/
+        [HttpGet]
+        public List<Emp_Model> Getc(string Cgdh, string Gys, string Pl, string Cgr, string Rkzt)
+        {
+            return ed.Show(Cgdh, Gys, Pl, Cgr, Rkzt);
+        }
+        [HttpGet]
+        public List<Emp_Model> GetAll()
+        {
+            return ed.GetAll();
+        }
+        [HttpGet]
+        public List<Emp_Model> GetXqy(int id)
+        {
+            return ed.GetXqy(id);
+        }
+        [HttpPost]
+        public int DelDhEmp(int id)
+        {
+            return ed.DelDhEmp(id);
+        }
+        public int UpEmp(string Gys, string Xhqy, string Cgsl, string Cgdj, string Je, string Sl, string Rq, string Bz, string id) {
+            return ed.UpEmp (Gys, Xhqy, Cgsl, Cgdj, Je, Sl, Rq, Bz, id);
+        }
+        public int AddEmp(string DhCgdh, string DhGys, string DhPl, string DhRq, string DhName, string DhType, string DhYlbm, string DhYlmc, string DhWq, string DhYlGg, string DhCgsl, string DhBz, string DhHq, string DhJe, string DhSl, string DhDj) {
+
+            return ed.AddDhEmp(DhCgdh, DhGys, DhPl, DhRq, DhName, DhType, DhYlbm, DhYlmc, DhWq, DhYlGg, DhCgsl, DhBz, DhHq, DhJe, DhSl, DhDj);
+        }
+    }
+}
