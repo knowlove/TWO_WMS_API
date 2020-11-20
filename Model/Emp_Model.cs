42fcc1dc3e81ad59f4d336c1a29d22fdeb4af054
刘浩闯小盆友 <@L>
2020-11-20 18:44:39 +0800

    22

---
+++ Model/Emp_Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Emp_Model
+    {
+
+        public int DhId         { get; set; }
+        public string DhCgdh    { get; set; }
+        public string DhGys     { get; set; }
+        public string DhPl      { get; set; }
+        public string DhRq      { get; set; }
+        public string DhName    { get; set; }
+        public string DhType    { get; set; }
+        public string DhYlbm    { get; set; }
+        public string DhYlmc    { get; set; }
+        public string DhWq      { get; set; }
+        public string DhYlGg    { get; set; }
+        public string DhCgsl    { get; set; }
+        public string DhBz      { get; set; }
+        public string DhHq      { get; set; }
+        public string DhJe      { get; set; }
+        public string DhSl      { get; set; }
+        public string DhDj      { get; set; }
+
+    }
+}
